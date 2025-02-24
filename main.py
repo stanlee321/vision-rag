625cf024a64c2a0f201f24587c9bb16dc11d3cc1
Stanley Salvatierra <s.salvatierra.bo@gmail.com>
2025-02-24 13:49:38 -0400

    fixed thread

--- main.py
+++ main.py
@@ -91,11 +91,6 @@ async def upload_endpoint(
     print(f"Document type: {doc_type}")
     print(f"Loader: {loader}")
     print(f"File: {file}")
-
-    # result = await asyncio.wait_for(
-    #     rag_api.upload_document(file, collection_name, doc_type, loader),
-    #     timeout=TIMEOUT
-    # )
     
     result = await asyncio.wait_for(
         asyncio.to_thread(rag_api.upload_document, file, collection_name, doc_type, loader),
